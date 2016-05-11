2e49583d803d7657ed1462ff1ea5bdc176f65966
SavK <jinmikei7@gmail.com>
2016-05-11 10:30:06 -0600

    Update PokemonGame.java

--- PokemonGame/src/pokemongame/PokemonGame.java
+++ PokemonGame/src/pokemongame/PokemonGame.java
@@ -15,7 +15,10 @@ public class PokemonGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Create model layer objects
+        
+        
+        
         
     }
     
