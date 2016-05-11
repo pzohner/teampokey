7d81f6668e556feecc6a6b4d7e2a170d737cc52b
savannahkei <savannahkei@192.168.170.1>
2016-05-11 10:27:13 -0600

    5/11/2016 Added project files

--- PokemonGame/src/pokemon/Pokemon.java
+++ PokemonGame/src/pokemon/Pokemon.java
@@ -3,20 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pokemongame;
+package pokemon;
 
 /**
  *
- * @author porterzohner
+ * @author savannahkei
  */
-public class PokemonGame {
+public class Pokemon {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
     }
     
 }
