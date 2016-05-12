96d7e712f39e37aea0e84ed295c97dfd581ff225
savannahkei <savannahkei@192.168.170.1>
2016-05-12 10:11:27 -0600

    Merge origin/master
    Conflicts:
    	PokemonGame/src/pokemongame/PokemonGame.java

---
+++ PokemonGame/src/byui/cit260/pokemongame/model/Player.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pokemongame.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author savannahkei
+ */
+public class Player implements Serializable {
+    
+    private String name;
+    private double timeCompleted; 
+
+    public Player() {
+    }
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getTimeCompleted() {
+        return timeCompleted;
+    }
+
+    public void setTimeCompleted(double timeCompleted) {
+        this.timeCompleted = timeCompleted;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.name);
+        hash = 97 * hash + (int) (Double.doubleToLongBits(this.timeCompleted) ^ (Double.doubleToLongBits(this.timeCompleted) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (Double.doubleToLongBits(this.timeCompleted) != Double.doubleToLongBits(other.timeCompleted)) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + ", timeCompleted=" + timeCompleted + '}';
+    }
+    
+    
+    
+}
