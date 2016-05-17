9b42d1c0a7de26a3b5cb92898451ac6ce705a1e4
savannahkei <savannahkei@192.168.170.1>
2016-05-17 09:35:46 -0600

    5/17 Added new classes model layer

---
+++ PokemonGame/src/byui/cit260/pokemongame/model/Pokeball.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pokemongame.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author savannahkei
+ */
+public class Pokeball implements Serializable {
+  
+
+private boolean isMaster; 
+
+    public Pokeball() {
+    }
+
+    
+    public boolean IsMaster() {
+        return isMaster;
+    }
+
+    public void setIsMaster(boolean isMaster) {
+        this.isMaster = isMaster;
+        
+    
+    }
+
+    @Override
+    public String toString() {
+        return "Pokeball{" + "isMaster=" + isMaster + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 43 * hash + (this.isMaster ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pokeball other = (Pokeball) obj;
+        if (this.isMaster != other.isMaster) {
+            return false;
+        }
+        return true;
+    }
+
+
+
+}
---
+++ PokemonGame/src/byui/cit260/pokemongame/model/Potion.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pokemongame.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+/**
+ * 
+ *
+ * @author savannahkei
+ */
+public class Potion implements Serializable {
+
+    private String healPower ; 
+
+    public Potion() {
+    }
+    
+ 
+
+    public String getHealPower() {
+        return healPower;
+    }
+
+    public void setHealPower(String healPower) {
+        this.healPower = healPower;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.healPower);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Potion other = (Potion) obj;
+        if (!Objects.equals(this.healPower, other.healPower)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Potion{" + "healPower=" + healPower + '}';
+    }
+
+  
+    
+    
+
+            
+    
+}
