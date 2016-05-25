fc866f047e249a9cb202b8a4c513d188592e3fd1
savannahkei <savannahkei@192.168.170.1>
2016-05-25 17:21:20 -0600

    5/17 Added new classes model layer

---
+++ PokemonGame/src/byui/cit260/pokemongame/control/BattleControl.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.pokemongame.control;
+
+import byui.cit260.pokemongame.model.Pokemon;
+import java.util.Random;
+
+/**
+ *
+ * @author savannahkei
+ */
+public class BattleControl {
+    
+    public int attack (Pokemon pokemonAttacker, Pokemon pokemonDefender){
+            
+if (pokemonAttacker.getStrength() < 1 || pokemonAttacker.getStrength() >20) {
+	return -1;
+}
+if (pokemonDefender.getDefense() <1 || pokemonDefender.getDefense()> 10){ 
+	return -1;
+}
+if (pokemonDefender.getHealth()>100 || pokemonDefender.getHealth()< 1) {
+        return -1;
+}
+
+Random rand = new Random(); 
+int randomValue = rand.nextInt(10)+1;
+
+int damageDealt = pokemonAttacker.getStrength() + randomValue-pokemonDefender.getDefense();
+int finalHealth = pokemonDefender.getHealth() - damageDealt;
+pokemonDefender.setHealth(finalHealth);
+    
+    return 0;
+    }
+}
+    
