419254632c01446520463c0a5e49b830fca2c444
porterzohner <porterzohner@10.18.151.125>
2016-06-06 10:04:43 -0600

    Finished team assignment of view layer

--- PokemonGame/src/byui/cit260/pokemongame/view/GameMenuView.java
+++ PokemonGame/src/byui/cit260/pokemongame/view/GameMenuView.java
@@ -5,14 +5,125 @@
  */
 package byui.cit260.pokemongame.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author porterzohner
  */
 public class GameMenuView {
 
-    void displayMenu() {
-        System.out.println("\n*** displayMenu() was called");
+    private String menu;
+    
+    public GameMenuView() {
+        
+        this.menu = "\n"
+                + "\n--------------------------------------------"
+                + "\n| Game Menu                                |"
+                + "\n--------------------------------------------"
+                + "\n V - View Map"
+                + "\n L - Item list"
+                + "\n P - Pokemon list"
+                + "\n E - Explore Square"
+                + "\n S - Save Game"
+                + "\n H - Help Menu"
+                + "\n Q - Quit"
+                + "\n--------------------------------------------";
+    }
+    
+    void displayGameMenu() {
+    
+        boolean done = false;
+        
+        do {
+            
+            String menuOption = getMenuOption();
+            
+            if(menuOption.toUpperCase().equals("Q")) {
+                return;        
+        }
+            
+            done = this.doAction(menuOption);
+            
+        } while (!done);
+    }
+
+    private String getMenuOption() {
+        
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid = false;
+        
+        while(!valid) {
+            //MainMenuView mainMenuView = new MainMenuView();
+            System.out.println(this.menu);
+        
+        value = keyboard.nextLine();
+        value = value.trim();
+        
+        if(value.length() < 1) {
+            System.out.println("\n Invalid value: value cannot be blank");
+            continue;
+            
+        }
+        break;
+    }
+     return value;  
+    }
+    
+
+    private boolean doAction(String choice) {
+        choice = choice.toUpperCase();
+        
+        switch (choice) {
+            case "V":
+                this.displayMap();
+                break;
+            case "L":
+                this.displayItemList();
+                break;
+            case "P":
+                this.displayPokemonList();
+                break;
+            case"E":
+                this.displayExploreSquare();
+                break;
+            case"S":
+                this.saveGame();
+                break;
+            case"H":
+                this.displayHelpMenu();
+                break;
+            default:
+                System.out.println("\n*** Invalid selection *** Try again");
+                break;
+            
+        }
+        return false;
+    }
+
+    private void displayMap() {
+        System.out.println("\n*** displaymap called");
+    }
+
+    private void displayItemList() {
+        System.out.println("\n*** displayItemList called");
+    }
+
+    private void displayPokemonList() {
+        System.out.println("\n*** displayPokemonList called");
+    }
+
+    private void displayExploreSquare() {
+        System.out.println("\n*** displayExploreSquare");
+    }
+
+    private void saveGame() {
+        System.out.println("\n*** saveGame");
+    }
+
+    private void displayHelpMenu() {
+        System.out.println("\n*** displayHelpMenu");
     }
     
 }
