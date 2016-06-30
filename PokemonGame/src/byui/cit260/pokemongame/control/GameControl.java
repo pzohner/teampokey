5d804c909d1d92862e949bfbeaa18afbde2aeaca
savannahkei <savannahkei@192.168.170.1>
2016-06-29 23:11:02 -0600

    5/17 Added new classes model layer

--- PokemonGame/src/byui/cit260/pokemongame/control/GameControl.java
+++ PokemonGame/src/byui/cit260/pokemongame/control/GameControl.java
@@ -14,6 +14,10 @@ import byui.cit260.pokemongame.model.Player;
 import byui.cit260.pokemongame.model.Pokeball;
 import byui.cit260.pokemongame.model.Potion;
 import byui.cit260.pokemongame.model.Scene;
+import byui.cit260.pokemongame.model.Pokemon;
+import byui.cit260.pokemongame.model.Character;
+
+
 import java.util.ArrayList;
 import pokemongame.PokemonGame;
 
@@ -45,35 +49,121 @@ public class GameControl {
         //Character character = new Character();
         Item item = new Item();
         
+        
+        
         PokemonGame.setCurrentGame(game);
         
         game.setPlayer(player);
         
-       //Character[] character = GameControl.createCharacter();
-       //game.setCharacter(character);
+        Character[] character = GameControl.createCharacters();
+        game.setCharacter(character);
+        
+        
                
-       //HOW DO WE IMPLEMENT THE ITEM OBJECTS WHEN ITEM CLASS RELATES TO CHARACTER CLASS
-       
-      Map map = MapControl.createMap();
+        // create the pokemon
+          Pokemon[] pokemon = GameControl.createPokemon();
+          Character[] characterWithList = game.getCharacter();
+          characterWithList[1].setPokemon(pokemon);
+          //Character[] characterWithList = game.getCharacter();
+        
+        
+        // create and set the map
+        Map map = MapControl.createMap();
         game.setMap(map);
         
-
-         ArrayList<Object> itemList = GameControl.createItemList();
-         item.setTotalItemList(itemList); 
+        // create and set the totalItemList
+        ArrayList<Object> itemList = GameControl.createItemList();
+        item.setTotalItemList(itemList); 
         
-   //     MapControl.moveCharactersToStartingLocation(map);
+        //MapControl.moveCharactersToStartingLocation(map);
+        
+    }
+    
+    public static Pokemon[] createPokemon() {
+        
+        Pokemon[] pokemon  = new Pokemon[6]; // total of 6 pokemon
+   
+        Pokemon pikachu   = new Pokemon("Pikachu"  , 20, 5,  65); 
+        Pokemon rattata   = new Pokemon("Rattata"  , 10, 1,  30); 
+        Pokemon spearow   = new Pokemon("Spearow"  , 27, 4,  50); 
+        Pokemon charizard = new Pokemon("Charizard", 30, 7,  85);
+        Pokemon gayardos  = new Pokemon("Gayardos" , 32, 8,  90);
+        Pokemon mewTwo    = new Pokemon("MewTwo"   , 35, 10, 100);
+        
+        pokemon[5] = pikachu;
+        pokemon[4] = rattata;
+        pokemon[3] = spearow;
+        pokemon[2] = charizard;
+        pokemon[1] = gayardos;
+        pokemon[0] = mewTwo;
+        
+//        pokemon[PokemonList.Pikachu.ordinal()]   = pikachu;
+//        pokemon[PokemonList.Rattata.ordinal()]   = rattata;
+//        pokemon[PokemonList.Spearow.ordinal()]   = spearow;
+//        pokemon[PokemonList.Charizard.ordinal()] = charizard;
+//        pokemon[PokemonList.Gayardos.ordinal()]  = gayardos;
+//        pokemon[PokemonList.MewTwo.ordinal()]    = mewTwo;
+        
+        for (int i = 0; i < pokemon.length; i++) {
+            
+            System.out.println(pokemon[i].getName());
+            
+        }
+        
+        
+        return pokemon;
+    }
+    
+    private enum PokemonList {
+        
+        Pikachu,
+        Rattata,
+        Spearow,
+        Charizard,
+        Gayardos,
+        MewTwo
         
     }
 
-    public static Character[] createCharacter() {
+    public static Character[] createCharacters() {
         
+        Character[] character = new Character[2];
+        Location[][] startingLocation = new Location[0][0];
+        Location[][] bossLocation = new Location[4][4];
+        ArrayList<Object> ashItemList = new ArrayList<>();
+        Pokemon[] pokemonList = new Pokemon[5];
         
+        // create ASh the player 
+        Character ash = new Character();
+        ash.setName("Ash");
+        ash.setLocation(startingLocation);
         
-                return null;
+        // set ash's itemList
+        ash.setItemList(ashItemList);
+        ash.countItemList();
+        ash.setPokemon(pokemonList);
+        character[CharacterList.Ash.ordinal()] = ash;
+        
+        // create LANCE the boss
+        Character lance = new Character();
+        lance.setName("Lance");
+        lance.setLocation(startingLocation);
+        lance.setItemList(ashItemList);
+        lance.countItemList();
+        lance.setPokemon(pokemonList);
+        character[CharacterList.Lance.ordinal()] = lance;
+        
+        return character;
+        
+    }
+    
+    public enum CharacterList {
+        
+        Ash,
+        Lance
     }
 
    
-
     private static ArrayList<Object> createItemList() {
         
         ArrayList<Object> itemList = new ArrayList<>();
@@ -105,16 +195,6 @@ public class GameControl {
         
     }
     
-    private void count()
-    {
-        ArrayList<Object> list = createItemList();
-        int count = 0;
-        
-        for(Object obj : list)
-            count++;
-        
-        System.out.println("The total items in the object list is: " + count);
-    }
     
     private enum ItemList {
         
@@ -123,8 +203,55 @@ public class GameControl {
         potion,
         superPotion
         
+    }
+    
+    public static Pokemon[] sortPokemonListAttackValues(Pokemon[] arrayToSort) {
         
-        // pokemon enum list?
         
+        int n = arrayToSort.length;
+        int k;
+        for (int m = n; m >= 0; m--) {
+            for (int i = 0; i < n - 1; i++) {
+                k = i + 1;
+                if (arrayToSort[i].getStrength() > arrayToSort[k].getStrength()) {
+                                       
+                    swapNumbers(i, k, arrayToSort);
+                }
+            }
+            //printNumbers(arrayToSort);
+            
+            
+        }
+        return arrayToSort;
+    }
+
+
+     private static void swapNumbers(int i, int j, Pokemon[] array) {
+  
+         
+        Pokemon temp;
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
     }
 }
+        
+//        for (int i = 0; i < arrayToSort.length - 1; i++)
+//        {
+//            int index = i;
+//            for (int j = i + 1; j < arrayToSort.length; j++)
+//                if (arrayToSort[j].getStrength() < arrayToSort[index].getStrength())
+//                    index = j;
+//      
+//            int smallerNumber = arrayToSort[index]; 
+//            arrayToSort[index] = arrayToSort[i];
+//            arrayToSort[i] = smallerNumber;
+//        }
+//        
+//        return arrayToSort;
+    
+        
+        
+    
+
